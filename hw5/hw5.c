ac4a0d05a529a491e8cebbb831174b646cf7f5f2
Krish Gupta <148490891+kgupta1149@users.noreply.github.com>
2025-12-10 12:30:16 -0500

    Refactor Vehicle interface to use Car struct

--- hw5/hw5.c
+++ hw5/hw5.c
@@ -1,73 +1,37 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
-/* ----------------------------------------
-   "Vehicle" interface simulated using 
-   function pointers.
-   ---------------------------------------- */
+typedef struct {
+    char owner[50];
+} Car;
 
-typedef struct Vehicle {
-    char name[50];
-
-    void (*startEngine)(struct Vehicle*);
-    void (*accelerate)(struct Vehicle*);
-    void (*brake)(struct Vehicle*);
-} Vehicle;
-
-/* ----------------------------------------
-   Functions for a Car
-   ---------------------------------------- */
-
-void car_startEngine(Vehicle* v) {
-    printf("%s: Engine started!\n", v->name);
+void startEngine(Car *c) {
+    (void)c;  
+    printf("Engine started!\n");
 }
 
-void car_accelerate(Vehicle* v) {
-    printf("%s: Car: Pressing gas pedal...\n", v->name);
+void accelerate(Car *c) {
+    (void)c;
+    printf("Car: Pressing gas pedal...\n");
 }
 
-void car_brake(Vehicle* v) {
-    printf("%s: Car: Applying brakes...\n", v->name);
+void brake(Car *c) {
+    (void)c;
+    printf("Car: Applying brakes...\n");
 }
 
-/* Constructor-like function */
-Vehicle* createCar(const char* name) {
-    Vehicle* v = malloc(sizeof(Vehicle));
+int main(void) {
+    Car janesCar;
+    Car jacksCar;
 
-    strcpy(v->name, name);
+    strcpy(janesCar.owner, "Jane's car");
+    strcpy(jacksCar.owner, "Jack's car");
 
-    v->startEngine = car_startEngine;
-    v->accelerate  = car_accelerate;
-    v->brake       = car_brake;
+    Car *myCar = &janesCar;
+    startEngine(myCar);
+    accelerate(myCar);
+    brake(myCar);
 
-    return v;
-}
-
-/* ----------------------------------------
-   main()
-   ---------------------------------------- */
-
-int main() {
-
-    /* Create "Car" objects */
-    Vehicle* janesCar = createCar("Jane's car");
-    Vehicle* jacksCar = createCar("Jack's car");
-
-    /* Behaves like the original C++ code */
-    janesCar->startEngine(janesCar);
-    janesCar->accelerate(janesCar);
-    janesCar->brake(janesCar);
-
-    printf("\n");  // separator
-
-    jacksCar->startEngine(jacksCar);
-    jacksCar->accelerate(jacksCar);
-    jacksCar->brake(jacksCar);
-
-    /* Free memory */
-    free(janesCar);
-    free(jacksCar);
 
     return 0;
 }
